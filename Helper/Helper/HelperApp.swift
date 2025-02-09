e43f754dda6ce2ff12465e73680fc1bc61a2cbef
KaterinaFomenko <katerinafomenko77@gmail.com>
2025-02-09 13:12:52 +0100

    First commit empty project

---
+++ Helper/Helper/HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelperApp.swift
+//  Helper
+//
+//  Created by Катерина Фоменко on 09/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
